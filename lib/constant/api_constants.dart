39f9ba804bdf71a131f501fb284f6d73da11b519
ghaem <ghaemaliabadi@gmail.com>
2023-11-17 18:34:49 +0330

    chore: apiUrl constant added to constants file

--- lib/constant/api_constants.dart
+++ lib/constant/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiUrlConstants {
   ApiUrlConstants._();
-  static const String baseUrl = ""; // with / at the end
+  static const String baseUrl = ""; // with / at the end of url
+  static const String apiUrl = "";
 }
